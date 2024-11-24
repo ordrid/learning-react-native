34c2a6107b36182cd0e725799b6f28cdd6d9eccb
Michael B. Espeña <espena.michael.pub@gmail.com>
2024-11-24 21:36:55 +0800

    learning `Image`, `ImageBackground` component
    - rendering image from URI and local file

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text><Text style={{ color: 'white'}}>Hello</Text> World</Text>
+      {/* <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      <Image
+        source={{ uri: "https://picsum.photos/300" }}
+        style={{ width: 300, height: 300 }}
+      /> */}
+
+      <ImageBackground source={logoImg} style={{ flex: 1, width: '100%' }}>
+        <Text>IMAGE TEXT</Text>
+      </ImageBackground>
     </View>
   );
 }
@@ -11,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'plum',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "plum",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
