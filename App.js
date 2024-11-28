7087cc37a141b9713cc150fef62113891f64d7b6
Michael B. Espeña <espena.michael.pub@gmail.com>
2024-11-28 12:26:12 +0800

    learning `Alert` component

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { ActivityIndicator, Alert, Button, StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ActivityIndicator />
-      <ActivityIndicator size="small" color="midnightblue" animating={false} />
-      <ActivityIndicator size="large" color="lightgreen" />
+      <Button title="Alert 1" onPress={() => { Alert.alert("Invalid data") }} />
+
+      <Button title="Alert 2" onPress={() => { Alert.alert("Invalid data", "Date of birth incorrect") }} />
+
+      <Button title="Alert 3" onPress={() => { Alert.alert("Invalid data", "Date of birth incorrect", [
+        {
+          text: "Cancel",
+          onPress: () => { console.log("Cancel pressed") }
+        },
+        {
+          text: "Okay",
+          onPress: () => { console.log("Okay pressed") }
+        }
+      ]) }} />
     </View>
   );
 }
@@ -17,5 +28,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     padding: 80,
+    gap: 8
   },
 });
